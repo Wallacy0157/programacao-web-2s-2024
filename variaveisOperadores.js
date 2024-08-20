6281391d6fcf2866514da7cfb5f540fd2550a8cc
Wallacy William dos Santos Oliveira <111467048+Wallacy0157@users.noreply.github.com>
2024-08-19 21:58:21 -0300

    Create variaveisOperadores.js

---
+++ variaveisOperadores.js
@@ -0,0 +1,24 @@
+let num1 = 10;
+let num2 = 10;
+let soma = num1 + num2;
+console.log(`Resultado da adição entre ${num1} e ${num2} é ${soma}`);
+
+let num3 = 10;
+let num4 = 5;
+let subtracao = num3 - num4;
+console.log(`Resultado da subtração entre ${num3} e ${num4} é ${subtracao}`);
+
+let num5 = 20;
+let num6 = 2;
+let multiplicacao = num5 * num6;
+console.log(`Resultado da multiplicação entre ${num5} e ${num6} é ${multiplicacao}`);
+
+let num7 = 50;
+let num8 = 5;
+let divisao = num7 / num8;
+console.log(`Resultado da divisão entre ${num7} e ${num8} é ${divisao}`);
+
+let num9 = 100;
+let num10 = 10;
+let modulo = num9 % num10;
+console.log(`Resultado do módulo entre ${num9} e ${num10} é ${modulo}`);
