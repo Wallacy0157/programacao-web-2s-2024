60ec83b6358002eb1ab4476f5d305a57402dba9b
Wallacy0157 <unicowallacy@gmail.com>
2024-08-30 10:19:15 -0300

    Atualização

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -20,3 +20,20 @@ console.log(verificarNumeroPrimo(100));
 console.log(verificarNumeroPrimo(991));
 console.log(verificarNumeroPrimo(104729));
 console.log(verificarNumeroPrimo(14348907));
+
+/*
+function verificarNumeroPrimo(n){
+    if(n < 1){
+        return false;
+    }
+    if((n > 2) && (n % 2 == 0)){
+        return false;
+    }
+    for(let i = 3; i  < n; i+=2){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+*/
