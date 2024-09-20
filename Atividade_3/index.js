fd66f26469858ee34b8144f11a5fa25743cc8255
Wallacy0157 <unicowallacy@gmail.com>
2024-09-20 09:20:07 -0300

    Ativiadade 3 concluida

--- Atividade_3/index.js
+++ Atividade_3/index.js
@@ -1,9 +1,36 @@
-const lc = require('./letter_case/letter_case');
-const express = requise('express');
-
+const express = require('express');
 const app = express();
+const calculadora = require('./util/calculadora');
 
-const PORT = 8080;
-app.listen(PORT, ()=>{
-    console.log('app rodando na porta' + PORT);
+app.get('/somar/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+    const resultado = calculadora.somar(a, b);
+    res.send(`Resultado: ${resultado}`);
+});
+
+app.get('/subtrair/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+    const resultado = calculadora.subtrair(a, b);
+    res.send(`Resultado: ${resultado}`);
+});
+
+app.get('/multiplicar/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+    const resultado = calculadora.multiplicar(a, b);
+    res.send(`Resultado: ${resultado}`);
+});
+
+app.get('/dividir/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+    const resultado = calculadora.dividir(a, b);
+    res.send(`Resultado: ${resultado}`);
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
